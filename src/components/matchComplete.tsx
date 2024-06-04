ec5fca151a1bea4e02be98c772ee391dc94a1e88
nebula <zqnftk5@naver.com>
2024-06-04 16:04:23 +0900

    fix code and css

--- src/components/matchComplete.tsx
+++ src/components/matchComplete.tsx
@@ -18,17 +18,21 @@ const MatchComplete: React.FC<MatchCompleteProps> = ({ mate, handleCancel }) => 
   const locationName = getLocationName(mate.location);
 
   return (
-    <div className="flex flex-col items-center">
-      <p className="mb-4">{`${mate.name} 님과 매칭되었어요!`}</p>
-      <p className="mb-4">{`${mate.name} 님은 지금 '${locationName}' 앞에서 기다리고 있습니다.`}</p>
+    <div className="flex flex-col items-center justify-center">
+      <div className='flex flex-col items-center justify-center p-6 border border-button rounded-lg shadow-md mb-6 mt-24'>
+        <p className="text-xl font-bold text-gray-700 mb-4">{`${mate.name} 님과 매칭되었어요!`}</p>
+        <p className="text-lg font-bold text-gray-700 mb-2">{`${mate.name} 님은`}</p>
+        <p className="text-lg font-bold text-button mb-2">{`'${locationName}'`}</p>
+        <p className="text-lg font-bold text-gray-700 mb-2">{`앞에서 기다리고 있습니다.`}</p>
+      </div>
       <button
-        className="w-full bg-orange-400 text-white py-2 rounded mb-2"
+        className="bg-button text-white font-semibold tracking-wider py-4 mt-32 mb-4 rounded w-full"
         onClick={handleCancel}
       >
         매칭 완료
       </button>
       <button
-        className="w-full bg-orange-400 text-white py-2 rounded"
+        className="bg-button text-white font-semibold tracking-wider py-4  rounded w-full"
         onClick={handleCancel}
       >
         취소하기
